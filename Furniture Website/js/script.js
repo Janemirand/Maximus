c91137126f0925e96b4d4b167b2a379f14164c88
Maximus <132723305+Janemirand@users.noreply.github.com>
2023-06-01 12:32:20 +0800

    Add files via upload

---
+++ Furniture Website/js/script.js
@@ -0,0 +1,24 @@
+let navbar = document.querySelector('.header .navbar');
+let menuBtn = document.querySelector('#menu-btn');
+
+menuBtn.onclick = () =>{
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+};
+
+window.onscroll = () =>{
+    menuBtn.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+var swiper = new Swiper(".home-slider", {
+    autoplay: {
+        delay: 4500,
+        disableOnInteraction: false,
+    },
+    grabCursor: true,
+    loop:true,
+});
+
+
+
+
